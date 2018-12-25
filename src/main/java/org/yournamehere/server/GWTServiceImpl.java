52143ee41bc92bf0452ce726ec4d70ba24690f52
Iter <Iter@com>
2018-12-25 17:31:28 +0300

    Delete through object

--- src/main/java/org/yournamehere/server/GWTServiceImpl.java
+++ src/main/java/org/yournamehere/server/GWTServiceImpl.java
@@ -1,5 +1,7 @@
 package org.yournamehere.server;
 
+import com.google.gwt.json.client.JSONObject;
+import com.google.gwt.json.client.JSONParser;
 import com.google.gwt.user.server.rpc.RemoteServiceServlet;
 import java.io.File;
 import java.io.FileWriter;
@@ -27,10 +29,17 @@ public class GWTServiceImpl extends RemoteServiceServlet implements GWTService {
         if (path == null) {
             path = getServletContext().getRealPath("books");
         }
-        String[] strs = s.split(":");
+        String[] strs;
+        String[] res;
 
-        String[] res = strs[1].split("&");
-
+        if (s.contains("{")) {
+            String[] str = {"rem", s};
+            strs = str;
+            res = str;
+        } else {
+            strs = s.split(":");
+            res = strs[1].split("&");
+        }
         if (strs[0].equals("add")) {
             if (books == null) {
                 books = getData(path);
@@ -40,7 +49,7 @@ public class GWTServiceImpl extends RemoteServiceServlet implements GWTService {
                     Integer.parseInt(res[3]), Integer.parseInt(res[4]),
                     new Date(), Double.parseDouble(res[6])));
         }//end add if
-        
+
         if (strs[0].equals("edt")) {
             if (books == null) {
                 books = getData(path);
@@ -52,7 +61,7 @@ public class GWTServiceImpl extends RemoteServiceServlet implements GWTService {
             AppData appNew = new AppData(res[1], res[2], res[0],
                     Integer.parseInt(res[3]), Integer.parseInt(res[4]),
                     new Date(Long.parseLong(res[5])), Double.parseDouble(res[6]));
-                Collections.replaceAll(books, app, appNew);
+            Collections.replaceAll(books, app, appNew);
         }//end add if
 
         if (strs[0].equals("rem")) {
@@ -60,17 +69,19 @@ public class GWTServiceImpl extends RemoteServiceServlet implements GWTService {
                 books = getData(path);
             }
 
-            books.remove(new AppData("123", "123", strs[1],
+            org.json.JSONObject js = new org.json.JSONObject(s);
+            books.remove(new AppData("123", "123", 
+                    js.getString("rem"),
                     Integer.parseInt("123"), Integer.parseInt("123"),
                     new Date(), Double.parseDouble("123")));
+            return s;
         }//end remove if
-        
-        if(strs[0].equals("int"))
-        {
+
+        if (strs[0].equals("int")) {
             if (books == null) {
                 books = getData(path);
             }
-            
+
             StringBuffer writeStr = new StringBuffer();
             for (GWTServiceUsageExample.AppData book : books) {
                 /*AppData(String name, String author, String isbn, int pages, int age, Date date, double price)*/
@@ -89,92 +100,77 @@ public class GWTServiceImpl extends RemoteServiceServlet implements GWTService {
                 writeStr.append(book.getPrice());
                 writeStr.append("\n");
             }
-            
+
             return writeStr.toString();
-            
+
         }//end init if
-        
-        if(strs[0].equals("srt"))
-        {
+
+        if (strs[0].equals("srt")) {
             if (books == null) {
                 books = getData(path);
             }
-            
-            if(strs[1].equals("isbn"))
-            {
-                Comparator<GWTServiceUsageExample.AppData> comp = new 
-                    Comparator<GWTServiceUsageExample.AppData>()
-                    {
+
+            if (strs[1].equals("isbn")) {
+                Comparator<GWTServiceUsageExample.AppData> comp = new Comparator<GWTServiceUsageExample.AppData>() {
                     @Override
                     public int compare(AppData o1, AppData o2) {
                         return o1.getIsbn().compareTo(o2.getIsbn());
-                    }};
+                    }
+                };
                 Collections.sort(books, comp);
             }
-            if(strs[1].equals("name"))
-            {
-                Comparator<GWTServiceUsageExample.AppData> comp = new 
-                    Comparator<GWTServiceUsageExample.AppData>()
-                    {
+            if (strs[1].equals("name")) {
+                Comparator<GWTServiceUsageExample.AppData> comp = new Comparator<GWTServiceUsageExample.AppData>() {
                     @Override
                     public int compare(AppData o1, AppData o2) {
                         return o1.getName().compareTo(o2.getName());
-                    }};
+                    }
+                };
                 Collections.sort(books, comp);
             }
-            if(strs[1].equals("author"))
-            {
-                Comparator<GWTServiceUsageExample.AppData> comp = new 
-                    Comparator<GWTServiceUsageExample.AppData>()
-                    {
+            if (strs[1].equals("author")) {
+                Comparator<GWTServiceUsageExample.AppData> comp = new Comparator<GWTServiceUsageExample.AppData>() {
                     @Override
                     public int compare(AppData o1, AppData o2) {
                         return o1.getAuthor().compareTo(o2.getAuthor());
-                    }};
+                    }
+                };
                 Collections.sort(books, comp);
             }
-            if(strs[1].equals("pages"))
-            {
-                Comparator<GWTServiceUsageExample.AppData> comp = new 
-                    Comparator<GWTServiceUsageExample.AppData>()
-                    {
+            if (strs[1].equals("pages")) {
+                Comparator<GWTServiceUsageExample.AppData> comp = new Comparator<GWTServiceUsageExample.AppData>() {
                     @Override
                     public int compare(AppData o1, AppData o2) {
                         return o1.getPages() - o2.getPages();
-                    }};
+                    }
+                };
                 Collections.sort(books, comp);
             }
-            if(strs[1].equals("age"))
-            {
-                Comparator<GWTServiceUsageExample.AppData> comp = new 
-                    Comparator<GWTServiceUsageExample.AppData>()
-                    {
+            if (strs[1].equals("age")) {
+                Comparator<GWTServiceUsageExample.AppData> comp = new Comparator<GWTServiceUsageExample.AppData>() {
                     @Override
                     public int compare(AppData o1, AppData o2) {
                         return o1.getAge() - o2.getAge();
-                    }};
+                    }
+                };
                 Collections.sort(books, comp);
             }
-            if(strs[1].equals("date"))
-            {
-                Comparator<GWTServiceUsageExample.AppData> comp = new 
-                    Comparator<GWTServiceUsageExample.AppData>()
-                    {
+            if (strs[1].equals("date")) {
+                Comparator<GWTServiceUsageExample.AppData> comp = new Comparator<GWTServiceUsageExample.AppData>() {
                     @Override
                     public int compare(AppData o1, AppData o2) {
                         return (int) (o1.getDate().getTime() - o2.getDate().getTime());
-                    }};
+                    }
+                };
                 Collections.sort(books, comp);
             }
-            if(strs[1].equals("price"))
-            {
-                Comparator<GWTServiceUsageExample.AppData> comp = new 
-                    Comparator<GWTServiceUsageExample.AppData>()
-                    {
+            if (strs[1].equals("price")) {
+                Comparator<GWTServiceUsageExample.AppData> comp = new Comparator<GWTServiceUsageExample.AppData>() {
                     @Override
                     public int compare(AppData o1, AppData o2) {
                         return (int) (o1.getPrice() - o2.getPrice());
-                    }};
+                    }
+                };
                 Collections.sort(books, comp);
             }
             StringBuffer writeStr = new StringBuffer();
@@ -195,11 +191,11 @@ public class GWTServiceImpl extends RemoteServiceServlet implements GWTService {
                 writeStr.append(book.getPrice());
                 writeStr.append("\n");
             }
-            
+
             return writeStr.toString();
-            
+
         }//end init if
-        
+
         setData(path, books);
         return strs[1];
     }//end myMethod
